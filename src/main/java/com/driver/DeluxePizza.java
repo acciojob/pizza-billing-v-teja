7da33f0aeeffb8febd3b982754b94dd76e931043
v-teja <tejaviswa95@gmail.com>
2022-12-11 21:30:32 +0530

    First commit

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -3,7 +3,6 @@ package com.driver;
 public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
-        super(isVeg);
-        // your code goes here
+        super(isVeg, 0);
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -2,13 +2,29 @@ package com.driver;
 
 public class Pizza {
 
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
     private int price;
     private Boolean isVeg;
     private String bill;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+        if(isVeg==true){
+            setPrice(300);
+        }else setPrice(400);
+
+        this.bill = String.valueOf(price);
+    }
+    public Pizza(Boolean isVeg, int i){
+        this.isVeg = isVeg;
+        if(isVeg==true){
+            setPrice(450);
+        }else setPrice(550);
+
+        this.bill = String.valueOf(price);
     }
 
     public int getPrice(){
@@ -16,15 +32,19 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-        // your code goes here
+        bill = String.valueOf(Integer.valueOf(bill)+80);
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        if(isVeg){
+            bill = String.valueOf(Integer.valueOf(bill)+70);
+        }else{
+            bill = String.valueOf(Integer.valueOf(bill)+120);
+        }
     }
 
     public void addTakeaway(){
-        // your code goes here
+        bill = String.valueOf(Integer.valueOf(bill)+20);
     }
 
     public String getBill(){
